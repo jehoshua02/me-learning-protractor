0f4fa71ad801752e23b72b13decc8171849f1c68
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-03-01 19:27:36 -0700

    Tutorial Step 2

--- tutorial/spec.js
+++ tutorial/spec.js
@@ -1,13 +1,33 @@
 // spec.js
 describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  });
 
-    element(by.id('gobutton')).click();
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 
-    expect(element(by.binding('latest')).getText()).
-        toEqual('3');
+  it('should add one and two', function() {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
   });
 });
